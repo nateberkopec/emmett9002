22b1e1c4b1a738a41c5ea6d4dbdaa021c2a288bc
Nate Berkopec <nate.berkopec@gmail.com>
2012-04-15 19:59:24 -0400

    Mode structure

--- server.rb
+++ server.rb
@@ -7,21 +7,64 @@ require 'gabbler'
 
 get '/' do
 	# show some generic stuff, documentation about mark, last tweet, etc
-	@tweet = generate_tweet
+	@tweet = generate_tweet(rand(7))
   haml :index
 end
 
-def generate_tweet
-	tweet = false
-	until tweet
+def generate_tweet(mode)
+	case mode
+  when 0..2
+    return sentence_mode
+  when 3..5
+    return word_mode
+  when 6
+    return tourettes_mode
+  end
+end
+
+def sentence_mode
+  tweet = false
+  until tweet
     begin
-      candidate = MARK.generate_n_words rand(15)
+      candidate = MARK.generate_1_sentence
     rescue
       next
     end
-  	if candidate.length < 140 && candidate.length > 1
-  		tweet = candidate
-  	end
+    if candidate.length < 140 && candidate.length > 20
+      tweet = candidate
+    end
   end
   return tweet
 end
+
+def word_mode
+  tweet = false
+  until tweet
+    begin
+      candidate = MARK.generate_n_words rand(5) - 2
+    rescue
+      next
+    end
+    if candidate.length < 140 && candidate.length > 1
+      tweet = candidate
+    end
+  end
+  return tweet
+end
+
+def tourettes_mode
+  tweet = false
+  until tweet
+    begin
+      candidate = MARK.generate_n_words rand(3)
+    rescue
+      next
+    end
+    if candidate.length < 140 && candidate.length > 1 && candidate != candidate.upcase
+      tweet = candidate.upcase
+    end
+  end
+  return tweet
+end
+
+
