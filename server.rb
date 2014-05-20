bea4aafd6cf8105263e0832a62dd00c8450639f8
Nate Berkopec <nate.berkopec@gmail.com>
2014-05-20 11:19:32 -0400

    change modes

--- server.rb
+++ server.rb
@@ -6,26 +6,19 @@ class Emmett
     @dark = MarkyMarkov::TemporaryDictionary.new
     @dark.parse_file 'config/blogposts.txt'
     @english = YAML.load(File.open('config/english.yaml'))
-    @funny_shit = *File.open('config/funny_words.txt')
   end
 
   def generate_tweet
-    case SecureRandom.random_number(17)
-    when 0..1
+    case SecureRandom.random_number(3)
+    when 0
       puts "TOURETTES"
       return tourettes_mode
-    when 2..9
-      puts 'blog'
-      return blog_mode
-    when 10..12
+    when 1
       puts 'haiku'
       return haiku_mode
-    when 13..15
+    when 2
       puts 'entity'
       return entity_replacement
-    when 16
-      puts 'funny shit'
-      return funny_shit
     end
   end
 
@@ -38,38 +31,9 @@ class Emmett
     end
   end
 
-  def blog_mode
-    candidate = ""
-    begin
-      rand(4).times do
-        candidate += @dark.generate_n_words rand(3) + 1
-        candidate += " "
-        candidate += @mark.generate_n_words rand(3) + 1
-        candidate += " "
-      end
-    rescue
-      nil
-    end
-    if (1..140) === candidate.length
-      tweet = candidate
-    else
-      blog_mode
-    end
-  end
-
   def haiku_mode
-    begin
-      candidate = case rand(3)
-      when 0
-        @dark.generate_n_words rand(2) + 2
-      when 1
-        @mark.generate_n_words rand(2) + 2
-      when 2
-        @dark.generate_1_word + " " + @mark.generate_1_word
-      end
-    rescue
-      nil
-    end
+    candidate = (@mark.generate_n_words rand(1) + 1) + " " + (@mark.generate_n_words rand(1) + 1)
+    candidate.split(" ").shuffle.join(" ")
     if (1..140) === candidate.length
       tweet = candidate
     else
@@ -104,29 +68,5 @@ class Emmett
       generate_entity
     end
 
-  end
-
-  def funny_shit
-    begin
-      candidate = case rand(2)
-      when 0
-        @mark.generate_1_word + " " + gimme_shit
-      when 1
-        gimme_shit + " " + @mark.generate_1_word
-      end
-    rescue
-      nil
-    end
-    if (1..140) === candidate.length
-      tweet = candidate
-    else
-      funny_shit
-    end
-  end
-
-  private
-
-  def gimme_shit
-    @funny_shit[rand(@funny_shit.length)].strip
   end
 end
